f34df07a8a797fb8b379737b779bf13f21ddecc6
Gabrieltgn6 <gabrieltgn6@gmail.com>
2023-12-24 17:33:54 +0100

    primera prueba generando qr

--- hola.py
+++ hola.py
@@ -1,3 +1,11 @@
-hola = "Hola Mundo"
+import qrcode
 
-print (hola)
+data = "Bienvenido"
+
+qr = qrcode.QRCode(version = 1, box_size=10, border = 5)
+
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill_color = "black", back_color = "white")
+
+img.save("C:/Users/gabri/Documents/Programacion/python/primer-hola/imagenes-qr/qr.png")
